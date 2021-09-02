dc80bfd147c1e366773fa9eaaee7457ac70fd911
Kemal Ozturk <51798197+ozturkkl@users.noreply.github.com>
2021-09-02 10:58:19 -0400

    Added files

---
+++ widget/setup.js
@@ -0,0 +1,26 @@
+export default {
+    defaultArtist: "My Radio",
+    defaultTitle: "No title found...",
+    defaultImage: "default.png",
+    currentlyPlayingTrack: "https://www.radioking.com/widgets/api/v1/radio/306541/track/current",
+    widgetSize: "300px",
+    infoWidth: "700px",
+    bodyBackground: "#050505",
+}
+
+const exampleResponse = { // EXAMPLE RADIOKING RESPONSE
+    "id": 26264416,
+    "artist": "Ruben Paz y Chévéréfusion",
+    "title": "Virgencita (Version Live Binaural)",
+    "album": "Salsa Para Todos",
+    "started_at": "2021-09-02T14:46:01+0000",
+    "end_at": "2021-09-02T14:50:01+0000",
+    "next_track": "2021-09-02T14:50:02+0000",
+    "duration": 240.00,
+    "buy_link":
+        "https://music.apple.com/us/album/virgencita-version-live-binaural/1410570548?app=itunes\u0026at=salsa.luna%40orange.fr\u0026i=1410571888",
+    "is_live": false,
+    "cover": "https://image.radioking.io/radios/306541/cover/id3/633d6b05-d8d8-421b-8bf5-d323be155484.jpg",
+    "default_cover": false,
+    "forced_title": false
+}
